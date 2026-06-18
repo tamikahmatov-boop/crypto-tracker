6d6368f58eabbf73b57649a5f54dba8f5b69d9fc
tamikahmatov-boop <tamikahmatov@gmail.com>
2026-06-18 20:42:36 +0300

    Integrate Telegram messaging and adjust sleep duration

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-print("🔥 PYTHON ЗАПУСТИЛСЯ")
-
+import requests
 import time
 
+BOT_TOKEN = "8626739818:AAFt7kmdfTgTVlXD-5FnKOVYq1fvNW9hUAw"
+CHAT_ID = "6716942872"
+
+print("START")
+
+r = requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    data={"chat_id": CHAT_ID, "text": "🧪 ТЕСТ РАБОТАЕТ"}
+)
+
+print(r.text)
+
 while True:
-    print("⏱ работает...")
-    time.sleep(5)
+    time.sleep(10)
